7c6906d74609c2ccb271de62945ce761b3f047a1
oopanpan <oopanpan@gmail.com>
2021-02-01 11:45:48 -0800

    Done

--- app.rb
+++ app.rb
@@ -2,5 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get('/reversename/:name'){
+    @name = params[:name]
+    "#{@name.split('').reverse.join}"
+  }
+
+  get('/square/:number'){
+    @number = params[:number].to_i
+    "#{(@number**2).to_s}"
+  }
+
+  get('/say/:number/:phrase'){
+    number = params[:number].to_i
+    phrase = params[:phrase]
+    "#{phrase}"*number
+  }
+
+  get('/say/:word1/:word2/:word3/:word4/:word5'){
+    w1 = params[:word1]
+    w2 = params[:word2]
+    w3 = params[:word3]
+    w4 = params[:word4]
+    w5 = params[:word5]
+    "#{w1+' '+w2+' '+w3+' '+w4+' '+w5}."
+  }
+
+  get('/:operation/:number1/:number2'){
+    operation = params[:operation]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    if operation == 'add'
+      "#{(num1+num2).to_s}"
+    elsif operation == 'subtract'
+      "#{(num1-num2).to_s}"
+    elsif operation == 'multiply'
+      "#{(num1*num2).to_s}"
+    elsif operation == 'divide'
+      "#{(num1/num2).to_s}"
+    end
+  }
 
 end

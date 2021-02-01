f29b64c7f96b4d75411e0e3ccdfb0d3857bc6375
oopanpan <oopanpan@gmail.com>
2021-02-01 11:34:47 -0800

    commit tree 050ada706ecf4cdf69ec182dfccb1021017ce523

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get('/reversename/:name'){
+    @name = params[:name]
+    "#{@name.split('').reverse.join}"
+  }
 
+  get('/square/:number'){
+    @number = params[:number].to_i
+    "#{(@number**2).to_s}"
+  }
+
+  get('/say/:number/:phrase'){
+    number = params[:number].to_i
+    phrase = params[:phrase]
+    "#{phrase}"*number
+  }
+
+  get('/say/:word1/:word2/:word3/:word4/:word5'){
+    w1 = params[:word1]
+    w2 = params[:word2]
+    w3 = params[:word3]
+    w4 = params[:word4]
+    w5 = params[:word5]
+    "#{w1+w2+w3+w4+w5}."
+  }
 end
